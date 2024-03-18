14a3ec666023af75ebd69f555ebe1d2aab510d25
Atunje <atunje_afolabi@yahoo.com>
2024-03-18 17:29:52 +0100

    [refactor] rewrite test descriptions for more clarity

--- src/__tests__/Cards.test.ts
+++ src/__tests__/Cards.test.ts
@@ -6,26 +6,31 @@ import Card from "../Card";
 const emptyDrawPile = [];
 const player1 = new Player(1, emptyDrawPile);
 
-player1.addToDiscardPile([
+/**
+ * Let's have something in the
+ * discard pile to begin with.
+ */
+const sampleCards = [
   new Card(1),
   new Card(2),
   new Card(3),
   new Card(4),
   new Card(5),
-]);
+];
+player1.addToDiscardPile(sampleCards);
 
 describe("a player with an empty draw pile tries to draw a card", () => {
   player1.drawCard();
 
-  test("discard pile should have a length of 0", () => {
+  test("discard pile should now be empty after drawing a card", () => {
     expect(player1.sizeOfDiscardPile()).toEqual(0);
   });
 
-  test("draw pile should have a length of 4", () => {
-    expect(player1.sizeOfDrawPile()).toEqual(4);
+  test("draw pile should now have a size of (sampleCards - 1) because one card was drawn", () => {
+    expect(player1.sizeOfDrawPile()).toEqual(sampleCards.length - 1);
   });
 
-  test("draw pile should been shuffled or not equal to discard pile", () => {
+  test("draw pile should be shuffled and therefore not equal to discard pile", () => {
     expect(player1.getDrawPile()).not.toEqual(player1.getDiscardPile());
   });
 });
